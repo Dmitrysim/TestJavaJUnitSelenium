2409ca29f36f0c15983b7200cbb6d966e13aa4ce
Dmitrysim <Dima_87771995@mail.ru>
2020-10-08 14:23:46 +0300

    2 test

--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -19,4 +19,18 @@ public class SeleniumTests
         Assert.assertEquals(expectedResult, actualResult);
         driver.quit();
     }
+
+    @Test
+    public void testHelloSemen()
+    {
+        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://lm.skillbox.ru/qa_tester/module01/");
+        driver.findElement(By.name("name")).sendKeys("Semen");
+        driver.findElement(By.className("button")).click();
+        var actualResult = driver.findElement(By.className("start-screen__res")).getText();
+        var expectedResult = "Привет, Vasya!";
+        Assert.assertEquals(expectedResult, actualResult);
+        driver.quit();
+    }
 }
