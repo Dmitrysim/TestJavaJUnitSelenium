b455407f90792cfdcf80008ec0ad98ac3eb7b0ad
Dmitrysim <Dima_87771995@mail.ru>
2020-10-16 17:59:56 +0300

    getting rid of duplication

--- src/test/java/HomeWorkWebDriver.java
+++ src/test/java/HomeWorkWebDriver.java
@@ -1,15 +1,32 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomeWorkWebDriver
 {
+    private WebDriver driver;
+
+    @Before
+    public void setUp()
+    {
+        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
     @Test
     public void testAllFieldsHello()
     {
-        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
-        var driver = new ChromeDriver();
         driver.navigate().to("https://lm.skillbox.ru/qa_tester/module02/homework1/");
         driver.findElement(By.name("name")).sendKeys("Ivanov Ivan Ivanovich");
         driver.findElement(By.name("email")).sendKeys("test@mail.ru");
@@ -20,14 +37,11 @@ public class HomeWorkWebDriver
                 "На вашу почту (test@mail.ru) отправлено письмо.\n" +
                 "Наш сотрудник свяжется с вами по телефону: +7989000000.";
         Assert.assertEquals(expectedText, actualText);
-        driver.quit();
     }
 
     @Test
     public void testEmptyFieldsHello()
     {
-        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
-        var driver = new ChromeDriver();
         driver.navigate().to("https://lm.skillbox.ru/qa_tester/module02/homework1/");
         driver.findElement(By.className("button")).click();
         var actualText = driver.findElement(By.className("start-screen__res")).getText();
@@ -35,6 +49,5 @@ public class HomeWorkWebDriver
                 "На вашу почту () отправлено письмо.\n" +
                 "Наш сотрудник свяжется с вами по телефону: .";
         Assert.assertEquals(expectedText, actualText);
-        driver.quit();
     }
 }
--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -1,4 +1,6 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -6,31 +8,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTests
 {
+    private WebDriver driver;
+
+    @Before
+    public void setUp()
+    {
+        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
     @Test
     public void testHelloVasya()
     {
-        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://lm.skillbox.ru/qa_tester/module01/");
         driver.findElement(By.name("name")).sendKeys("Вася");
         driver.findElement(By.className("button")).click();
         var actualResult = driver.findElement(By.className("start-screen__res")).getText();
         var expectedResult = "Привет, Вася!";
         Assert.assertEquals(expectedResult, actualResult);
-        driver.quit();
     }
 
     @Test
     public void testHelloSemen()
     {
-        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://lm.skillbox.ru/qa_tester/module01/");
         driver.findElement(By.name("name")).sendKeys("Semen");
         driver.findElement(By.className("button")).click();
         var actualResult = driver.findElement(By.className("start-screen__res")).getText();
         var expectedResult = "Привет, Vasya!";
         Assert.assertEquals(expectedResult, actualResult);
-        driver.quit();
     }
 }
