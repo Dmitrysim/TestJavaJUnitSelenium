f908ae0fb792bbc7f8637bd1c1909857168a2425
Dmitrysim <Dima_87771995@mail.ru>
2020-10-16 18:26:38 +0300

    test for online cinema added

---
+++ src/test/java/TestOnlineCinema.java
@@ -0,0 +1,49 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestOnlineCinema
+{
+    private WebDriver driver;
+
+    @Before
+    public void setUp()
+    {
+        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
+    @Test
+    public void testMailSend()
+    {
+        driver.navigate().to("https://lm.skillbox.ru/qa_tester/module06/register/");
+        driver.findElement(By.name("name")).sendKeys("Андрей");
+        driver.findElement(By.name("email")).sendKeys("skillbox@test.ru");
+        driver.findElement(By.name("password")).sendKeys("qwerty!123");
+        driver.findElement(By.className("form-submit")).click();
+        var actualText = driver.findElement(By.className("form-result")).getText();
+        var expectedText = "Вам на почту skillbox@test.ru отправлено письмо";
+        Assert.assertEquals(expectedText, actualText);
+    }
+
+    @Test
+    public void testWrongPassword()
+    {
+        driver.navigate().to("https://lm.skillbox.ru/qa_tester/module06/register/");
+        driver.findElement(By.className("form-submit")).click();
+        var actualText = driver.findElement(By.className("form-error")).getText();
+        var expectedText = "Введите имя";
+        Assert.assertEquals(expectedText, actualText);
+    }
+}
